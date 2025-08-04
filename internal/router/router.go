3b41aa105bebdb8dc9f6809cc8e8cf667b4f8cd0
Pklerik <pklerik@mail.ru>
2025-08-04 19:31:50 +0300

    [new] iter3 - added graceful shutdown mechanism

--- internal/router/router.go
+++ internal/router/router.go
@@ -2,23 +2,50 @@
 package router
 
 import (
-	"errors"
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/Pklerik/urlshortener/internal/handler"
+	"golang.org/x/sync/errgroup"
 )
 
 // StartServer starts server with base configuration.
 func StartServer() {
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go func() {
+		c := make(chan os.Signal, 1) // we need to reserve to buffer size 1, so the notifier are not blocked
+		signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+		<-c
+		cancel()
+	}()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc(`/`, handler.MainPage)
 
-	log.Println("Starting server")
-
-	if err := http.ListenAndServe(`:8080`, mux); !errors.Is(err, http.ErrServerClosed) {
-		log.Fatalf("HTTP server error: %v", err)
+	httpServer := &http.Server{
+		Addr:    ":8000",
+		Handler: mux,
 	}
 
-	log.Println("Stopped serving new connections.")
+	g, gCtx := errgroup.WithContext(ctx)
+	g.Go(func() error {
+		log.Println("Starting server")
+		return httpServer.ListenAndServe()
+	})
+	g.Go(func() error {
+		<-gCtx.Done()
+		log.Println("Stopped serving new connections.")
+		return httpServer.Shutdown(context.Background())
+	})
+
+	if err := g.Wait(); err != nil {
+		log.Printf("exit reason: %s \n", err)
+	}
 }
